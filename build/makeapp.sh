ea4e3d9b1d3f14a58ab48ec50a1224807e9e1b85
Ben White <hello@bemm.com.au>
2018-05-28 18:58:14 +1000

    commit apps to repo in attempt to use brew cask

--- build/makeapp.sh
+++ build/makeapp.sh
@@ -3,23 +3,26 @@
 set -e
 
 find include -name '*.sh' -exec sh -c '
+  cleanup() { [ -e "$1" ] && rm -R "$1"; }
   filepath="$1"
   filename="${filepath##*/}"
   file="${filename%.*}"
   source "${filepath%.sh}.cfg"
   # Remove app from this dir if it exists
-  [ -e "${name}.app" ] && rm -R "${name}.app"
+  cleanup "${name}.app"
+  cleanup "app/${name}.app"
   ./lib/appify.sh \
     --script "${filepath}" \
     --name "${name}" \
     --icons "icon/${file}.icns"
+  cp -r "${name}.app" "app/${name}.app"
   # Remove app from /Applications dir if it exists, and we want to overwrite
   if [ -e "/Applications/${name}.app" ]; then
     echo "/Applications/${name}.app already exists. Do you want to replace it?"
     select yn in "Yes" "No"; do
         case $yn in
-            Yes ) rm -R "/Applications/${name}.app"; break;;
-            No ) rm -R "${name}.app"; exit;;
+            Yes ) cleanup "/Applications/${name}.app"; break;;
+            No ) cleanup "${name}.app"; exit;;
         esac
     done
   fi
